c52e06e5481dc3a9089e86b227c97514d91574e9
Awesome Stickz <awesome@stickz.dev>
2023-03-30 23:45:30 +0530

    Added codes for inserting, updating tokens

---
+++ index.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const superagent = require('superagent');
+
+const func = async () => {
+    const tokens = fs
+        .readFileSync('./tokens')
+        .toString()
+        .split('\n')
+        .map((token) => token.replace(/\r|\s/g, ''));
+
+    let sql = `insert INTO custom_bots (bot_id, token)\nVALUES `;
+    let inviteLinks = '';
+
+    for (const token of tokens) {
+        if (!token) continue;
+
+        const botID = Buffer.from(token.split('.')[0], 'base64').toString();
+
+        const { bot_public, flags } = (await superagent.get('https://discord.com/api/oauth2/applications/@me').set('Authorization', `Bot ${token}`)).body;
+
+        if (!bot_public) return console.log(`Bot not public: Invalid intents for: https://discord.com/developers/applications/${botID}/bot`);
+        if (flags !== 557056) return console.log(`Invalid intents for: https://discord.com/developers/applications/${botID}/bot`);
+
+        sql += `('${botID}', '${token}'),\n`;
+        inviteLinks += `https://discord.com/api/oauth2/authorize?client_id=${botID}&permissions=0&scope=bot&guild_id=882993059720232990\n`;
+    }
+
+    sql = sql.slice(0, -2) + ';';
+
+    fs.writeFileSync('./addbots.sql', sql);
+    fs.writeFileSync('./invitelinks', inviteLinks);
+};
+
+func();
---
+++ updatetokens.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const superagent = require('superagent');
+
+const func = async () => {
+    const tokens = fs
+        .readFileSync('./tokens')
+        .toString()
+        .split('\n')
+        .map((token) => token.replace(/\r|\s/g, ''));
+
+    let sql = '';
+    let inviteLinks = '';
+
+    for (const token of tokens) {
+        if (!token) continue;
+
+        const botID = Buffer.from(token.split('.')[0], 'base64').toString();
+
+        const { bot_public, flags } = (await superagent.get('https://discord.com/api/oauth2/applications/@me').set('Authorization', `Bot ${token}`)).body;
+
+        if (!bot_public) return console.log(`Bot not public: Invalid intents for: https://discord.com/developers/applications/${botID}/bot`);
+        if (flags !== 557056) return console.log(`Invalid intents for: https://discord.com/developers/applications/${botID}/bot`);
+
+        sql += `update custom_bots set token = '${token}', is_token_invalid = null where bot_id = '${botID}';\n`;
+    }
+
+    fs.writeFileSync('./updatebots.sql', sql);
+    fs.writeFileSync('./invitelinks', inviteLinks);
+};
+
+func();
